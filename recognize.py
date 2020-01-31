c6240b3a026cc4e93b94bffeab970e9f7fb6fe93
ankit <1994constant@gmail.com>
2020-01-31 12:18:47 +0530

    commit

---
+++ recognize.py
@@ -0,0 +1,33 @@
+import azure.cognitiveservices.speech as speechsdk
+
+# Creates an instance of a speech config with specified subscription key and service region.
+# Replace with your own subscription key and service region (e.g., "westus").
+speech_key, service_region = "4328416405b945e3ab1f47db87c0e1e2", "eastus"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+# Creates a recognizer with the given settings
+speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+print("Say something...")
+
+
+# Starts speech recognition, and returns after a single utterance is recognized. The end of a
+# single utterance is determined by listening for silence at the end or until a maximum of 15
+# seconds of audio is processed.  The task returns the recognition text as result.
+# Note: Since recognize_once() returns only a single utterance, it is suitable only for single
+# shot recognition like command or query.
+# For long-running multi-utterance recognition, use start_continuous_recognition() instead.
+result = speech_recognizer.recognize_once()
+result1 = speech_synthesizer.speak_text_async(result.text).get()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+    print("Recognized: {}".format(result.text))
+elif result.reason == speechsdk.ResultReason.NoMatch:
+    print("No speech could be recognized: {}".format(result.no_match_details))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        print("Error details: {}".format(cancellation_details.error_details))
---
+++ speak.py
@@ -0,0 +1,26 @@
+import azure.cognitiveservices.speech as speechsdk
+
+# Creates an instance of a speech config with specified subscription key and service region.
+# Replace with your own subscription key and service region (e.g., "westus").
+speech_key, service_region = "4328416405b945e3ab1f47db87c0e1e2", "eastus"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+# Receives a text from console input.
+print("Type some text that you want to speak...")
+text = input()
+
+# Synthesizes the received text to speech.
+# The synthesized speech is expected to be heard on the speaker with this line executed.
+result = speech_synthesizer.speak_text_async(text).get()
+
+# Checks result.
+if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized to speaker for text [{}]".format(text))
+elif result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
