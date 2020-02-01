ce83a4fb75eb40f96e6e3192d83a7b378d815e18
stealthanthrax <sansyrox@gmail.com>
2020-02-01 09:06:22 +0530

    Translate added

--- client/index.js
+++ client/index.js
@@ -307,3 +307,27 @@ try {
         "Unfortunately, this browser/OS combination is unsupported. For best results, use Google Chrome on Desktop or Android."
     );
 }
+
+
+async function postData(url = '', data = {}) {
+    // Default options are marked with *
+    const response = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+    return await response.json(); // parses JSON response into native JavaScript objects
+  }
+  
+  postData('https://api.cognitive.microsofttranslator.com/translate?api-version=3.0', { "Text":"I would really like to drive your car around the block a few times."})
+    .then((data) => {
+      console.log(data); // JSON data parsed by `response.json()` call
+    });
